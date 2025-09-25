297153750636a86b8263472201bcf0849be1b4a4
Iliyan Georgiev <georgiev.iliyan09@gmail.com>
2025-09-25 21:42:22 +0300

    fixes

--- beaches/admin.py
+++ beaches/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
-from . import models
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, UserProfile, Beach, BeachLog, BeachReport
 
-# Register your models here.
-admin.site.register(models.UserProfile)
-admin.site.register(models.Beach)
-admin.site.register(models.BeachLog)
-admin.site.register(models.BeachReport)
+class CustomUserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets + (
+        (None, {'fields': ('is_first_login',)}),
+    )
+    
+    # Also, update the 'add_fieldsets' for the 'Add User' page
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        (None, {'fields': ('is_first_login',)}),
+    )
+
+@admin.register(User)
+class UserAdmin(CustomUserAdmin):
+    pass
+
+admin.site.register(UserProfile)
+admin.site.register(Beach)
+admin.site.register(BeachLog)
+admin.site.register(BeachReport)
