98b37cb432ad23e96052063207303dab70399181
Iliyan Georgiev <georgiev.iliyan09@gmail.com>
2025-11-14 21:41:15 +0200

    update

--- sea_sight/settings.py
+++ sea_sight/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['nautiq.onrender.com']
+ALLOWED_HOSTS = ["nautiq.up.railway.app"]
 
 # Application definition
 
