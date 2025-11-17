ff51e6d47ae51ed1711c38e91e3359230132ffb9
Iliyan Georgiev <georgiev.iliyan09@gmail.com>
2025-11-17 22:31:55 +0200

    fix

--- sea_sight/wsgi.py
+++ sea_sight/wsgi.py
@@ -1,5 +1,5 @@
 from PIL import Image
 from beaches.ai.clip_recognizer import get_clip_match
 
-dummy_image = Image.new("RGB", (1, 1))
+dummy_image = Image.new("L", (224, 224))
 get_clip_match(dummy_image, "dummy")
