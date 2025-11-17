0a69278243d7c7b85315394332231059c819a723
Iliyan Georgiev <georgiev.iliyan09@gmail.com>
2025-11-17 22:32:48 +0200

    Dummy image fix

--- sea_sight/wsgi.py
+++ sea_sight/wsgi.py
@@ -1,5 +1,5 @@
 from PIL import Image
 from beaches.ai.clip_recognizer import get_clip_match
 
-dummy_image = Image.new("L", (224, 224))
+dummy_image = Image.new("RGB", (224, 224), color=(128, 128, 128))
 get_clip_match(dummy_image, "dummy")
