4a95706b3959942b6c7fe0a1e6679b37635c84a1
Iliyan Georgiev <georgiev.iliyan09@gmail.com>
2025-11-27 20:33:28 +0200

    Deployment

--- staticfiles/js/togglePasswordButton.js
+++ staticfiles/js/togglePasswordButton.js
@@ -1,32 +1,36 @@
-const inputElements = document.querySelectorAll('input[type="password"]')
-const inputElement = inputElements.length > 0 ? inputElements[0] : null; 
-const toggleBtn = document.getElementById('toggle')
+const inputElements = document.querySelectorAll('input[type="password"]');
+const inputElement = inputElements.length > 0 ? inputElements[0] : null;
+const toggleBtn = document.getElementById('toggle');
 
-const forms = document.getElementsByTagName('form')
+const forms = document.getElementsByTagName('form');
 const form = forms.length > 0 ? forms[0] : null;
 
 const hidePassword = () => {
     if (inputElement) {
-        inputElement.type = "password"
-        toggleBtn.className = "bi bi-eye-fill toggle-btn"
+        inputElement.type = "password";
+        toggleBtn.className = "bi bi-eye-fill toggle-btn";
     }
-}
+};
 
 if (inputElement && toggleBtn) {
-    toggleBtn.addEventListener('mousedown', () => {
-        inputElement.type = 'text'
-        toggleBtn.className = 'bi bi-eye-slash-fill toggle-btn'
-    })
-    toggleBtn.addEventListener('mouseup', hidePassword)
-    toggleBtn.addEventListener('mouseleave', hidePassword)
 
-    toggleBtn.addEventListener('touchstart', () => {
-        inputElement.type = 'text'
-        toggleBtn.className = 'bi bi-eye-slash-fill toggle-btn'
-    })
-    toggleBtn.addEventListener('touchend', hidePassword)
+    toggleBtn.addEventListener('mousedown', () => {
+        inputElement.type = 'text';
+        toggleBtn.className = 'bi bi-eye-slash-fill toggle-btn';
+    });
+    toggleBtn.addEventListener('mouseup', hidePassword);
+    toggleBtn.addEventListener('mouseleave', hidePassword);
+
+    toggleBtn.addEventListener('touchstart', (e) => {
+        e.preventDefault();
+        inputElement.type = 'text';
+        toggleBtn.className = 'bi bi-eye-slash-fill toggle-btn';
+    });
+
+    toggleBtn.addEventListener('touchend', hidePassword);
+    toggleBtn.addEventListener('touchcancel', hidePassword);
 }
 
 if (form) {
-    form.addEventListener('submit', hidePassword)
+    form.addEventListener('submit', hidePassword);
 }
